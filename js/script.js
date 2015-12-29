fda628a173f7f1118cf26bd651a50adadcd35626
songshu189 <songshu.yu@yahoo.com>
2015-12-29 09:55:56 -0500

    Add one more cat, now two cats

--- js/script.js
+++ js/script.js
@@ -1,4 +1,18 @@
-$('#cat').on('click', function() {
-    var num = $('#clicks').text();
-    $('#clicks').text(parseInt(num)+1);
+cats = ['poplinre', 'chewie'];
+clicks = [2, 3];
+
+var container = $('#cat-container');
+
+for(var i=0; i < 2; i++) {
+    $('<div />').css('width', '50%')
+        .append('<h2>'+ cats[i] + '</h2>')
+        .append('<img src="images/' + cats[i] + '.jpg" />')
+        .append('<h2 class="click">' + clicks[i] + '</h2>')
+        .appendTo(container);
+}
+
+$('img').on('click', function() {
+    var click = $(this).parent().find('.click');
+    var count = parseInt(click.text()) + 1;
+    click.text(count);
 });
