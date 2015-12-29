ec538829006afa533c3aa966f64b2cdd4fc724a0
songshu189 <songshu.yu@yahoo.com>
2015-12-29 11:00:07 -0500

    Premium: 5 cats and detail view

--- js/script.js
+++ js/script.js
@@ -1,18 +1,35 @@
-cats = ['poplinre', 'chewie'];
-clicks = [2, 3];
+cats = ['poplinre', 'chewie', 'adora', 'beauty', 'calico'];
+clicks = [2, 3, 5, 3, 6];
+var currentIdx = 0;
 
-var container = $('#cat-container');
+var catList = $('#cat-list');
 
-for(var i=0; i < 2; i++) {
-    $('<div />').css('width', '50%')
-        .append('<h2>'+ cats[i] + '</h2>')
-        .append('<img src="images/' + cats[i] + '.jpg" />')
-        .append('<h2 class="click">' + clicks[i] + '</h2>')
-        .appendTo(container);
+for(var i=0; i < cats.length; i++) {
+    $('<li />', {'cat-idx': i}).append('<h2>'+ cats[i] + '</h2>')
+        .appendTo(catList);
+}
+$('#cat-detail').append('<h2 id="cat-name">'+ cats[i] + '</h2>')
+        .append('<img id="cat-img" src="images/' + cats[i] + '.jpg" />')
+        .append('<h2 id="cat-click" class="click">' + clicks[i] + '</h2>');
+
+var catName = $('#cat-name');
+var catImg = $('#cat-img');
+var catClick = $('#cat-click');
+
+function setDetail() {
+    catName.text(cats[currentIdx]);
+    catImg.attr('src', 'images/' + cats[currentIdx] + '.jpg');
+    catClick.text(clicks[currentIdx]);
 }
 
+setDetail();
+
+catList.on('click', 'li', function() {
+    currentIdx = $(this).attr('cat-idx');
+    setDetail();
+});
+
 $('img').on('click', function() {
-    var click = $(this).parent().find('.click');
-    var count = parseInt(click.text()) + 1;
-    click.text(count);
+    clicks[currentIdx] += 1;
+    catClick.text(clicks[currentIdx]);
 });
