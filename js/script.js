72a97e0ef3d1a78b83cf6471b845449a55a5c452
songshu189 <songshu.yu@yahoo.com>
2016-01-04 20:18:21 -0500

    Reimplement with knockout.js

--- js/script.js
+++ js/script.js
@@ -1,139 +1,96 @@
-$(function() {
-    var data = [
-        {name: 'adora', 
+var data = [
+        {name: 'adora',
          imgurl: 'images/adora.jpg',
-         count: 2},
+         count: 2,
+         nicknames: ['Adora','Dora Cat']},
         {name: 'beauty',
          imgurl: 'images/beauty.jpg',
-         count: 1},
-        {name: 'calico', 
+         count: 1,
+         nicknames: ['Betty', 'Betty Cat']},
+        {name: 'calico',
          imgurl: 'images/calico.jpg',
-         count: 3},
-        {name: 'chewie', 
+         count: 3,
+         nicknames: ['Kelly', 'Lico Cat']},
+        {name: 'chewie',
          imgurl: 'images/chewie.jpg',
-         count: 5},
-        {name: 'kate', 
+         count: 5,
+         nicknames: ['Chevy', 'Chevy Cat']},
+        {name: 'kate',
          imgurl: 'images/kate.jpg',
-         count: 2},
-        {name: 'kitten', 
+         count: 2,
+         nicknames: ['Kate', 'Katty', 'Katty Cat']},
+        {name: 'kitten',
          imgurl: 'images/kitten.jpg',
-         count: 6},
-        {name: 'poplinre', 
+         count: 6,
+         nicknames: ['Kity', 'Kitty Cat']},
+        {name: 'poplinre',
          imgurl: 'images/poplinre.jpg',
-         count: 1},
-        {name: 'santaria', 
+         count: 1,
+         nicknames: ['Pop', 'Lina']},
+        {name: 'santaria',
          imgurl: 'images/santaria.jpg',
-         count: 3}
-    ];
+         count: 3,
+         nicknames: ['Santa', 'Sarry']}
+];
+
+var Cat = function(data) {
+    this.clickCount = ko.observable(data.count);
+    this.name = ko.observable(data.name);
+    this.imgurl = ko.observable(data.imgurl);
+    //this.imgAttribution = ko.observable(data.imgAttribution);
+
+    this.title = ko.computed(function(){
+        var clicks = this.clickCount();
+        if (clicks < 10)
+            return 'Newborn';
+        else if(clicks<20)
+            return 'Infant';
+        else if(clicks<50)
+            return 'Child';
+        else if(clicks<100)
+            return 'Teen';
+        else if(clicks<200)
+            return Adult;
+        return 'Ninja';
+    }, this);
+
+    this.nicknames = ko.observableArray(data.nicknames);
+};
+
+var ViewModel = function() {
+    var self = this;
     
-    var octopus = {
-        currItem: 0,
-        init: function() {
-            listView.init();
-            detailView.init();
-            adminView.init();
-        },
-        get: function() {
-            return data[this.currItem];
-        },
-        getAll: function() {
-            return data;
-        },
-        set: function(curr) {
-            this.currItem = curr;
-        },
-        click: function() {
-            data[this.currItem].count += 1;
-        },
-        update: function(name, url, count) {
-            var cat = this.get();
-            cat.name = name;
-            cat.imgurl = url;
-            cat.count = parseInt(count);
-        }
-    };
+    this.catList = ko.observableArray([]);
 
-    var listView = {
-        init: function() {
-            this.ulist = $('#cat-list');
-            this.render();
-
-            this.ulist.on('click', 'li', function( event ) {
-                octopus.set($( this ).attr('id'));
-                detailView.render();
-            });
-        },
-        render: function() {
-            this.ulist.html('');
-            var _self = this;
-            $.each(octopus.getAll(), function(i, cat) {
-                var $li = $('<li />', { id: i});
-                $li.append('<h3>' + cat.name + '</h3>');
-                _self.ulist.append($li);
-            });
-        }
-    };
-    
-    var detailView = {
-        init: function() {
-            var cat = octopus.get();
-
-            $('#cat-detail').append('<h2 id="cat-name">'+ cat.name + '</h2>')
-                .append('<img id="cat-img" src="images/' + cat.name + '.jpg" />')
-                .append('<h2 id="cat-click" class="click">' + cat.count + '</h2>');
-        
-            this.catName = $('#cat-name');
-            this.catClick = $('#cat-click');
-            this.catImg = $('#cat-img');
-            this.render();
-
-            this.catImg.on('click', function() {
-                octopus.click();
-                detailView.render();
-            });
-        },
-
-        render: function() {
-            var cat = octopus.get();
-            this.catName.html(cat.name);
-            this.catClick.html(cat.count);
-            this.catImg.attr('src',  cat.imgurl);
-        }
-    };
-   
-    var adminView = {
-        init: function() {
-            this.adminPanel = $('#admin-panel');
-            this.catName = $('#cat_name');
-            this.imgURL = $('#imgurl');
-            this.catClicks = $('#cat_clicks');
-            var _self = this;
-
-            $('#admin-btn').on('click', function() {
-                _self.adminPanel.show();
-                adminView.render();
-            });
-
-            this.adminPanel.hide();
-
-            $('#cancel-btn').on('click', function() {
-                _self.adminPanel.hide();
-            });
-
-            $('#save-btn').on('click', function() {
-                 octopus.update(_self.catName.val(), _self.imgURL.val(), _self.catClicks.val());
-                 detailView.render();
-                 listView.render();
-                _self.adminPanel.hide();
-            });
-        },
-        render: function() {
-            var cat = octopus.get();
-            this.catName.val(cat.name);
-            this.imgURL.val(cat.imgurl);
-            this.catClicks.val(cat.count);
-        }
+    for(var i=0; i<data.length; i++) {
+        //var cat = ko.observable();
+        this.catList().push(new Cat(data[i]));
     }
-   
-    octopus.init();
-});
+
+    this.currentCat = ko.observable(this.catList()[0]);
+
+    this.setCurrent = function(data) {
+        //console.log(this.currentCat);
+        this.currentCat(data);
+
+        //console.log(data);
+    }
+    this.incrementCounter = function() {
+        this.currentCat().clickCount(this.currentCat().clickCount() + 1);
+    };
+
+    this.show = ko.observable(false);
+    this.showPanel = function() {
+        self.show(true);
+    };
+    
+    this.hide = function() {
+        self.show(false);
+    };
+    
+    this.save = function() {
+        self.show(false);
+    }
+};
+
+ko.applyBindings(new ViewModel());
